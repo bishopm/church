94e3005b37977ecad2d39cd6935db37e4256be2a
Michael Bishop <michael@bishop.net.za>
2023-05-23 13:00:25 +0200

    Initial plugin files

---
+++ Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Bishopm\Church;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
---
+++ lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Church',
+        'description' => 'WinterCMS plugin for local church community management'
+    ]
+];

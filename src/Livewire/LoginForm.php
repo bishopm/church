c022c6deeab606739fcdb11d2fd8c859815699c5
Michael Bishop <michael@bishop.net.za>
2024-10-10 17:05:55 +0200

    Add new users

--- src/Livewire/LoginForm.php
+++ src/Livewire/LoginForm.php
@@ -2,13 +2,17 @@
  
 namespace Bishopm\Church\Livewire;
 
+use Bishopm\Church\Models\Household;
 use Bishopm\Church\Models\Individual;
 use Livewire\Component;
  
 class LoginForm extends Component
 {
     public $phone = '';
+    public $surname = '';
+    public $firstname = '';
     public $message = '';
+    public $addmessage = '';
     public $error = '';
     public $pin = '';
     public $hashed = '';
@@ -16,6 +20,7 @@ class LoginForm extends Component
     public $userpin = '';
     public $result = '';
     public $final = false;
+    public $status = "start";
  
     public function send(){
         if (strlen($this->phone) !== 10){
@@ -23,19 +28,43 @@ class LoginForm extends Component
         } elseif (!is_numeric($this->phone)) {
             $this->error = "Error: Only numbers - no letters should be included";
         } else {
+            $this->status= "phoneok";
             $indiv=Individual::where('cellphone',$this->phone)->get();
             if (count($indiv)>1){
                 $this->error="This number is associated with more than one person. Please contact our office and we'll help you to complete your registration.";
+                $this->status="toomany";
             } elseif (count($indiv)==1){
                 $this->error = "";
                 $this->message = "Hello, " . $indiv[0]->firstname . "! We are sending you an SMS now.";
                 $this->pin = rand(1000,9999);
                 $this->hashed = hash('sha256', $this->pin);
-                $this->pinsent = true;
+                $indiv[0]->uid=$this->hashed;
+                $indiv[0]->save();
+                $this->status = "pinsent";
             } else {
                 $this->error = "";
-                $this->message = "We don't have your name in our system yet. Please enter your name and press OK";
+                $this->addmessage = "We don't have your details in our system yet. Please enter your name and press OK";
+                $this->status="addind";
             }
+        }
+    }
+
+    public function addindiv(){
+        if (($this->firstname=='') or ($this->surname=='')){
+            $this->error = "Error: Please enter a first name and surname<br>";
+        } else {                
+            $household = Household::create(['addressee'=>$this->firstname . " " . $this->surname]);
+            $this->hashed = hash('sha256', $this->pin);
+            $indiv=Individual::create([
+                'firstname'=>$this->firstname,
+                'surname'=>$this->surname,
+                'cellphone'=>$this->phone,
+                'uid'=>$this->hashed,
+                'household_id'=>$household->id
+            ]);
+            $this->message = "Hello, " . $indiv->firstname . "! We are sending you an SMS now.";
+            $this->pin = rand(1000,9999);
+            $this->status = "pinsent";
         }
     }
 
--- src/Resources/views/livewire/login.blade.php
+++ src/Resources/views/livewire/login.blade.php
@@ -1,22 +1,43 @@
 <div>
     <h3>Member login</h3>
-    @if(!$pinsent)
+    @if($status<>"pinsent")
         <div data-mdb-input-init class="form-outline mb-4">
-            <label class="form-label">Enter your cellphone number</label>
-            <input wire:model="phone" class="form-control" placeholder="Cellphone number" />
-            <span class="text-warning">{{$error}}</span><span class="text-success">{{$message}}</span>
+            @if ($status<>"phoneok")
+                <label class="form-label">Enter your cellphone number</label>
+            @else
+                <h4>Phone: {{$phone}}</h4>
+            @endif
+            <input 
+                @if($status=="phoneok")type="hidden"@endif
+                wire:model="phone" 
+                class="form-control my-2" 
+                placeholder="Cellphone number" 
+            />
+            <span class="text-warning">{!!$error!!}</span><span class="text-success">{{$addmessage}}</span><span class="text-success">{{$message}}</span>
+            @if($addmessage)
+                <input wire:model="firstname" class="form-control my-2" placeholder="First name" />
+                <input wire:model="surname" class="form-control my-2" placeholder="Surname" />
+            @endif
             <div>{{$pin}}</div>
         </div>
-        <div class="text-center pt-1 mb-5 pb-1">
-            <button class="btn btn-dark btn-block fa-lg gradient-custom-2 mb-3" type="button" wire:click="send">
-                Send verification SMS to this number
-            </button>
-        </div>
+        @if (($status<>"toomany") and ($status<>"addind"))
+            <div class="text-center pt-1 mb-5 pb-1">
+                <button class="btn btn-dark btn-block fa-lg gradient-custom-2 mb-3" type="button" wire:click="send">
+                    Send verification SMS to this number
+                </button>
+            </div>
+        @elseif ($status=="addind")
+            <div class="text-center pt-1 mb-5 pb-1">
+                <button class="btn btn-dark btn-block fa-lg gradient-custom-2 mb-3" type="button" wire:click="addindiv">
+                    OK
+                </button>
+            </div>
+        @endif
     @endif
-    @if($pinsent)
+    @if($status=="pinsent")
         <div data-mdb-input-init class="form-outline mb-4">
             <label class="form-label">Enter PIN number</label>
-            <input wire:model="userpin" class="form-control" placeholder="Enter the code we sent you" />
+            <input wire:model="userpin" class="form-control" placeholder="Please enter the code we sent you" />
             <div>{{$pin}}</div>
         </div>
         <div class="text-center pt-1 mb-5 pb-1">

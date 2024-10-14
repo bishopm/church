cf4d981c937bf59379d1f21f330341637aef4a8d
Michael Bishop <michael@bishop.net.za>
2024-10-14 14:07:26 +0200

    scanner

--- src/Resources/views/components/website/layout.blade.php
+++ src/Resources/views/components/website/layout.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="width=device-width, initial-scale=1.0" user-scalable="no" name="viewport">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{setting('general.church_name')}} - {{$pageName}}</title>
   <meta name="description" content="">
--- src/Resources/views/website/books.blade.php
+++ src/Resources/views/website/books.blade.php
@@ -1,7 +1,46 @@
 <x-church::website.layout pageName="Books">
+    <style>
+        #camera video{
+            width:100%;
+            max-width: 640px;
+        }
+    </style>
     <h1>Books</h1>
+    @if(count($member))
+        <div id="camera" style="width:100%"></div>
+    @endif
     @foreach ($books as $book)
         <div><a href="{{url('/')}}/books/{{$book->id}}">{{$book->title}}</a> {{$book->allauthors}} </div>
     @endforeach
     {{$books->links()}}
+    <script src="https://cdn.jsdelivr.net/npm/@ericblade/quagga2/dist/quagga.min.js"></script>
+    <script>
+        const quaggaConf = {
+            inputStream: {
+                target: document.querySelector("#camera"),
+                // new
+                type: "LiveStream",
+                constraints: {
+                    width: { min: 640 },
+                    height: { min: 480 },
+                    facingMode: "environment",
+                    aspectRatio: { min: 1, max: 2 }
+                }
+            },
+            decoder: {
+                readers: ['code_128_reader']
+            },
+        }
+    
+        Quagga.init(quaggaConf, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            Quagga.start();
+        });
+    
+        Quagga.onDetected(function (result) {
+            alert("Detected barcode: " + result.codeResult.code);
+        });
+    </script>
 </x-church::layout>                

777bb79affcd7a0f55ec758a09f118b3e8d7f918
Michael Bishop <michael@bishop.net.za>
2024-10-18 12:40:53 +0200

    Changing themes

--- src/Resources/views/components/website/1layout.blade.php
+++ src/Resources/views/components/website/1layout.blade.php
@@ -8,89 +8,91 @@
   <title>{{setting('general.church_name')}} - {{$pageName}}</title>
   <meta name="description" content="">
   <meta name="keywords" content="">
+
   <!-- Favicons -->
   <link href="/public/church/images/icons/favicon.png" rel="icon">
   <link href="/public/church/images/icons/apple-touch-icon.png" rel="apple-touch-icon">
+
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+  
+  <!-- Vendor CSS Files -->
+  <link href="/public/church/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/public/church/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="/public/church/vendor/aos/aos.css" rel="stylesheet">
+  <link href="/public/church/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="/public/church/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
   <!-- Media player -->
   <link rel="stylesheet" href="https://cdn.vidstack.io/player/theme.css" />
   <link rel="stylesheet" href="https://cdn.vidstack.io/player/audio.css" />
   <script src="https://cdn.vidstack.io/player" type="module"></script>
+  <!-- Main CSS File -->
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
   <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-  <link rel="stylesheet" href="/public/church/css/leaflet.css">
-  <link rel="stylesheet" href="/public/church/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="/public/church/css/custom.css">
+  <link href="/public/church/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Active
+  * Template URL: https://bootstrapmade.com/active-bootstrap-website-template/
+  * Updated: Aug 07 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
   @laravelPWA
 </head>
 
-<body>
-  <div class="">
-    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
-      <div class="container-fluid">
-        <a href="{{url('/')}}" class="navbar-brand">
-          <!-- Uncomment the line below if you also wish to use an image logo -->
-          <!-- <img src="/public/church/img/logo.png" alt=""> -->
-          <h1 class="sitename">{{setting('general.church_abbreviation')}}</h1>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+<body class="index-page">
 
-        <div class="collapse navbar-collapse" id="navbarColor02">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <a class="nav-link active" href="{{url('/')}}#about">Home
-                <span class="visually-hidden">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{url('/')}}#sundays">Sundays</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{url('/')}}#blog">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{url('/')}}#connecting">Connecting</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{url('/')}}#serving">Getting involved</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{url('/')}}#contact">Contact</a>
-            </li>
-            @if (!count($member))
-              <li>
-                <a class="nav-link" href="{{url('/')}}/login"></a>Login
-              </li>
-            @else
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Members</a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="{{url('/')}}/mymenu">My menu</a>
-                  <a class="dropdown-item" href="{{url('/')}}/books">Books</a>
-                </div>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Resources</a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="{{url('/')}}/blog">Blog archive</a>
-                  <a class="dropdown-item" href="{{url('/')}}/sermons">Sermon archives</a>
-                </div>
-              </li>
-            @endif          
+  <header id="header" class="dark-background header d-flex align-items-center sticky-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+      <a href="{{url('/')}}" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="/public/church/img/logo.png" alt=""> -->
+        <h1 class="sitename">{{setting('general.church_abbreviation')}}</h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="{{url('/')}}#about" class="active">Home</a></li>
+          <li><a href="{{url('/')}}#sundays">Sundays</a></li>
+          <li><a href="{{url('/')}}#blog">Blog</a></li>
+          <li><a href="{{url('/')}}#connecting">Connecting</a></li>
+          <li><a href="{{url('/')}}#serving">Getting involved</a></li>
+          <li><a href="{{url('/')}}#contact">Contact</a></li>
+          @if (!count($member))
+          <li><a href="{{url('/')}}/login">Login</a></li>
+          @else
+          <li class="dropdown"><a href="{{url('/')}}/mymenu"><span>Members</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+          <ul>
+            <li><a href="{{url('/')}}/mymenu">My menu</a></li>
+            <li><a href="{{url('/')}}/books">Books</a></li>
           </ul>
-          <form class="d-flex">
-            <input class="form-control me-sm-2" type="search" placeholder="Search">
-            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-  </div>
+          @endif
+          <li class="dropdown"><a href="#"><span>More</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="{{url('/')}}#faqs">FAQ's</a></li>
+              <li><a href="{{url('/')}}/giving">Giving</a></li>
+              <li><a href="{{url('/')}}/groups">Groups</a></li>
+              <li><a href="{{url('/')}}/projects">Mission projects</a></li>
+              <li class="dropdown"><a href="#"><span>Resources</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="{{url('/')}}/blog">Blog archives</a></li>
+                  <li><a href="{{url('/')}}/sermons">Sermon archives</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+    @laravelPWA
+  </header>
+
   <main class="main">
     @if($pageName<>"Home")
     <div class="container my-3">
@@ -181,6 +183,11 @@
       <div class="copyright d-flex flex-column flex-md-row align-items-center justify-content-md-between">
         <p>© Westville Methodist Church {{date('Y')}}</p>
         <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you've purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+          <a style="color:lightgray"; target="_blank" href="https://bootstrapmade.com/">BootstrapMade</a>
         </div>
       </div>
     </div>
@@ -192,8 +199,14 @@
   <!-- Preloader -->
   <div id="preloader"></div>
   <!-- Vendor JS Files -->
-  <script src="/public/church/js/bootstrap-bundle.min.js"></script>
-  <script src="/public/church/js/custom.js"></script>
+  <script src="/public/church/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/public/church/vendor/aos/aos.js"></script>
+  <script src="/public/church/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="/public/church/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="/public/church/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="/public/church/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="/public/church/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="/public/church/js/main.js"></script>
 </body>
 
 </html>

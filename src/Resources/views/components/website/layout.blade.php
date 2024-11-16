fbc0cfaa2178b5fe51387a1ea84f732bdd71c4ab
Michael Bishop <michael@bishop.net.za>
2024-11-16 17:00:25 +0200

    Remove PWA blade

--- src/Resources/views/components/website/layout.blade.php
+++ src/Resources/views/components/website/layout.blade.php
@@ -21,7 +21,6 @@
   <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/{{setting('website.theme')}}/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="/public/church/css/custom.css">
-  @laravelPWA
 </head>
 
 <body>

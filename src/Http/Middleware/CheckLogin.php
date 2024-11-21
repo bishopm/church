e9e1f3c22a74acd1ac226003fe5f59f03a00b859
Michael Bishop <michael@bishop.net.za>
2024-11-21 18:03:37 +0200

    Various

--- src/Http/Middleware/CheckLogin.php
+++ src/Http/Middleware/CheckLogin.php
@@ -5,7 +5,10 @@ namespace Bishopm\Church\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+<<<<<<< HEAD
 use Illuminate\Support\Facades\Route;
+=======
+>>>>>>> 01ebd4a (Various)
 
 class CheckLogin
 {
@@ -16,6 +19,7 @@ class CheckLogin
      */
     public function handle(Request $request, Closure $next): Response
     {
+<<<<<<< HEAD
         $route = Route::getRoutes()->match($request);
         if ($route->getName() <> 'app.login'){
             if (!isset($_COOKIE['wmc-mobile']) or (!isset($_COOKIE['wmc-access']))){
@@ -26,5 +30,12 @@ class CheckLogin
         /* $response=$next($request);
         $response->headers->set('Cache-Control', '');
         return $response; */
+=======
+        if (!isset($_COOKIE['wmc-mobile']) or (!isset($_COOKIE['wmc-access']))){
+            return redirect('/app/login');
+        }
+
+        return $next($request);
+>>>>>>> 01ebd4a (Various)
     }
 }

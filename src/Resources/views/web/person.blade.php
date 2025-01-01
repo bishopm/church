59e390e7be18cc2d6cded6a678e0dedab7b033be
Michael Bishop <michael@bishop.net.za>
2025-01-01 13:08:39 +0200

    People pics

--- src/Resources/views/web/person.blade.php
+++ src/Resources/views/web/person.blade.php
@@ -1,7 +1,7 @@
 <x-church::website.layout pageName="People">
     <h1>{{$person->fullname}}</h1>
     <div style="min-height: 270px;">
-        <img style="float:left; padding-right:10px;" height="250px" src="{{url('/storage/' . $person->image)}}" alt="Image" class="rounded">
+        <img style="float:left; padding-right:10px;" height="250px" src="{{url('/storage/app/media/images/' . $person->image)}}" alt="Image" class="rounded">
         {{$person->bio}}<br><br>
         <a href="{{url('/')}}/sermons/{{$person->slug}}">Sermons: {{count($person->sermons)}}</a><br>
         <a href="{{url('/')}}/blog/{{$person->slug}}">Blog posts: {{count($person->posts)}}</a>

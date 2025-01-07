f9fddfb68235f2d0ba6a5db1cd8d82c7e803f4a3
Michael Bishop <michael@bishop.net.za>
2025-01-07 19:32:19 +0200

    Birthday widget

--- src/Filament/Widgets/Birthdays.php
+++ src/Filament/Widgets/Birthdays.php
@@ -23,33 +23,17 @@ class Birthdays extends Widget
         $fri=strval(date('m-d', strtotime("next Monday")+345600));
         $sat=strval(date('m-d', strtotime("next Monday")+432000));
         $sun=strval(date('m-d', strtotime("next Monday")+518400));
-        $msg="<b>Birthdays for the week: (starting " . $thisyr . "-" . $mon . ")</b><br><br>";
+        $msg="<b>Birthdays for the week: (starting " . $thisyr . "-" . $mon . ")</b><br>";
         $days=array($mon,$tue,$wed,$thu,$fri,$sat,$sun);
         $birthdays=Individual::join('households', 'households.id', '=', 'individuals.household_id')->wherein(DB::raw('substr(birthdate, 6, 5)'), $days)->whereNull('individuals.deleted_at')->select('individuals.firstname', 'individuals.surname', 'individuals.cellphone', 'households.homephone', 'households.householdcell', DB::raw('substr(birthdate, 6, 5) as bd'))->orderByRaw('bd')->get();
+        $olddate="";
         foreach ($birthdays as $bday) {
-            $msg=$msg . "<b>" . date("D d M", strtotime($thisyr . "-" . $bday->bd)) . "</b> " . $bday->firstname . " " . $bday->surname . ":";
-            if ($bday->cellphone) {
-                $msg=$msg . " Cellphone: " . $bday->cellphone;
+            if ($olddate<>date("D d M", strtotime($thisyr . "-" . $bday->bd))){
+                $msg=$msg . "<br><b>" . date("D d M", strtotime($thisyr . "-" . $bday->bd)) . "</b> " . $bday->firstname . " " . $bday->surname;
+                $olddate=date("D d M", strtotime($thisyr . "-" . $bday->bd));
+            } else {
+                $msg=$msg . ", " . $bday->firstname . " " . $bday->surname;
             }
-            if ($bday->homephone) {
-                $msg=$msg . " Homephone: " . $bday->homephone;
-            }
-            if (($bday->householdcell) and ($bday->householdcell<>$bday->id)) {
-                $msg=$msg . " Household cellphone: " . self::gethcell($bday->householdcell);
-            }
-            $msg=$msg . "<br>";
-        }
-        $anniversaries=Anniversary::join('households', 'households.id', '=', 'anniversaries.household_id')->select('homephone', 'householdcell', 'addressee', 'household_id', 'anniversarytype', 'details', DB::raw('substr(anniversarydate, 6, 5) as ad'))->wherein(DB::raw('substr(anniversarydate, 6, 5)'), $days)->orderBy(DB::raw('substr(anniversarydate, 6, 5)'))->get();
-        $msg = $msg . "<br>" . "<b>Anniversaries</b>" . "<br><br>";
-        foreach ($anniversaries as $ann) {
-            $msg=$msg . date("D d M", strtotime($thisyr . "-" . $ann->ad)) . " (" . $ann->addressee . ". " . ucfirst($ann->anntype) . ": " . $ann->details. ")";
-            if ($ann->homephone) {
-                $msg=$msg . " Homephone: " . $ann->homephone;
-            }
-            if ($ann->householdcell) {
-                $msg=$msg . " Household cellphone: " . self::gethcell($ann->householdcell);
-            }
-            $msg=$msg. "<br>";
         }
         $this->memberdata['msg']=$msg;
     }

24d52fedca2f44cbf2faa39839b98676aae83591
Michael Bishop <michael@bishop.net.za>
2025-01-13 12:06:39 +0200

    Change queue mgt

--- src/Providers/ChurchServiceProvider.php
+++ src/Providers/ChurchServiceProvider.php
@@ -121,7 +121,7 @@ class ChurchServiceProvider extends ServiceProvider
             ]);
         }
         $this->callAfterResolving(Schedule::class, function (Schedule $schedule) {
-            $schedule->command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
+            $schedule->command('queue:work --stop-when-empty')->withoutOverlapping();
             $schedule->command('churchsite:birthdayemail')->weekly()->fridays()->at('6:30');
             // $schedule->command('churchsite:maintenanceemail')->weekly()->mondays()->at('6:00');
             // $schedule->command('churchsite:monthlymeasures')->monthlyOn(1, '5:30');

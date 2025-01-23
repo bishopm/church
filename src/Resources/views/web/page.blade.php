669aedd2606ac0803e837de386dc899c71a0900f
Michael Bishop <michael@bishop.net.za>
2025-01-23 14:33:55 +0200

    Page image link fix

--- src/Resources/views/web/page.blade.php
+++ src/Resources/views/web/page.blade.php
@@ -2,7 +2,7 @@
     <h1>{{$page->title}}</h1>
     <div style="min-height: 270px;">
         @if ($page->image)
-            <img style="float:right; padding-left:10px;" height="250px" src="{{url('/storage/page/' . $page->image)}}" alt="Image" class="rounded">
+            <img style="float:right; padding-left:10px;" height="250px" src="{{url('/' . $page->image)}}" alt="Image" class="rounded">
         @endif
         {!!$page->content!!}<br><br>
     </div>

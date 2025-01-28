a3e3b8808bf440f019a9691ff470799cb72abedf
Michael Bishop <michael@bishop.net.za>
2025-01-28 22:20:28 +0200

    Email minutes

--- src/Filament/Clusters/Property/Resources/MeetingResource/Pages/EditMeeting.php
+++ src/Filament/Clusters/Property/Resources/MeetingResource/Pages/EditMeeting.php
@@ -4,6 +4,8 @@ namespace Bishopm\Church\Filament\Clusters\Property\Resources\MeetingResource\Pa
 
 use Bishopm\Church\Filament\Clusters\Property\Resources\MeetingResource;
 use Bishopm\Church\Http\Controllers\ReportsController;
+use Bishopm\Church\Models\Group;
+use Bishopm\Church\Models\Meeting;
 use Filament\Actions;
 use Filament\Forms\Components\Textarea;
 use Filament\Resources\Pages\EditRecord;
@@ -30,58 +32,20 @@ class EditMeeting extends EditRecord
     }
 
     public function sendMinutes($data){
-        //dd($data);
+        $message=$data['message'];
         $report=new ReportsController();
-        $minutespdf=$report->minutes(1);
-        $recipients = Group::with('individuals')->where('id',$roster)->get();
-        $rost = Rostermodel::find($roster);
+        $id=$this->record->id;
+        $meeting=Meeting::find($id);
+        $recipients = Group::with('individuals')->where('id',$meeting->group_id)->first();
+        $minutespdf=$report->minutes($id);
         $indivs = array();
-        foreach ($groups as $group){
-            if (isset($group->group)){
-                foreach ($group->group->groupmembers as $member){
-                    if (!isset($indivs[$member->individual_id])){
-                        $indivs[$member->individual_id]=array();
-                    }
-                    $indivs[$member->individual_id][]=['group_id'=>$group->group->id,'groupname'=>$group->group->groupname,'video'=>$group->video];
-                }
-            }
-        }
-        $emailcount=0;
-        $rostertitle=date('F',strtotime($firstday)) . " and " . date('F Y',strtotime($nextmonth));
         foreach ($indivs as $id=>$indiv){
-            $person = Individual::find($id);
-            $message = "Thank you so much for your willingness to serve at WMC. Attached is the service roster for " . $rostertitle . ".<br>";
-            foreach ($indiv as $group){
-                $message.="<br><b>" . $group['groupname'] . "</b><br>";
-                $message.="If you are not able to serve on any given day, please contact one of the other members of the team below and arrange to swap duties:<br><br>";
-                $team = Group::with('groupmembers.individual')->where('id',$group['group_id'])->first();
-                foreach ($team->groupmembers as $tm){
-                    if ($tm->individual->id <> $id){
-                        $message.=$tm->individual->firstname . " " . $tm->individual->surname . " (" . $tm->individual->cellphone . ")<br>";
-                    }
-                }
-                if ($group['video']) {
-                    $message.="<br>If you are new to the team, or have not seen it yet, have a look at the <b><a href=\"" . $group['video'] . "\">training video</a></b> we have prepared for this team. We hope it is helpful - please share any feedback you may have to help us improve!";
-                }
-            }
-            $message.="<br>May God bless you as you serve him here. Thank you!";
-            if ($person->email){
-                $dat=array();
-                $dat['content'] = $message;
-                $dat['title'] = $rost->roster . ": " . $rostertitle . " Roster";
-                $dat['church'] = Settings::get('church_name');
-                $dat['sender'] = Settings::get('smtpemail');
-                $dat['firstname'] = $person->firstname;
-                $dat['surname'] = $person->surname;
-                $dat['email'] = $person->email;
-                $emailcount++;
-                Mail::send('bishopm.churchsite::mail.general', $dat, function ($msg) use ($dat,$rosterpdf) {
-                    $msg->from(Settings::get('smtpemail'), Settings::get('church_name'));
-                    $msg->subject($dat['title']);
-                    $msg->to($dat['email']);
-                    $msg->attachData($rosterpdf, 'WMC_roster.pdf', ['mime' => 'application/pdf']);
-                });
-            }
+            Mail::send('bishopm.churchsite::mail.general', $dat, function ($msg) use ($dat,$minutespdf) {
+                $msg->from(Settings::get('smtpemail'), Settings::get('church_name'));
+                $msg->subject($dat['title']);
+                $msg->to($dat['email']);
+                $msg->attachData($minutespdf, 'minutes.pdf', ['mime' => 'application/pdf']);
+            });
         }
         return "Emails sent: " . $emailcount;
     }

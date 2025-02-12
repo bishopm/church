e0cea1ec0581d6b44f2c85f5d474dfd6a6df8098
Michael Bishop <michael@bishop.net.za>
2025-02-12 13:56:52 +0200

    Fix sermons link

--- src/Resources/views/app/sermons.blade.php
+++ src/Resources/views/app/sermons.blade.php
@@ -3,7 +3,7 @@
     @forelse ($series as $serie)
         <div class="row pb-3">
             <div class="col-12">
-                <a href="{{url('/')}}/sermon/{{date('Y',strtotime($serie->startingdate))}}/{{$serie->slug}}">
+                <a href="{{url('/')}}/sermons/{{date('Y',strtotime($serie->startingdate))}}/{{$serie->slug}}">
                     <img style="float: left; padding-right:10px;" width="400px" src="{{url('/storage/' . $serie->image)}}" alt="Image" class="img-fluid rounded">
                     <h4>{{$serie->series}} <small style="color:grey;">{{date('F Y',strtotime($serie->startingdate))}}</small></h4>
                 </a>

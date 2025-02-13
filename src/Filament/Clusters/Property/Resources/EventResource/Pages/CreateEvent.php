7bf3aea95080e3d7aa6da93368fdf75613056e4a
Michael Bishop <michael@bishop.net.za>
2025-02-13 13:41:01 +0200

    Add events to Google Calendar

--- src/Filament/Clusters/Property/Resources/EventResource/Pages/CreateEvent.php
+++ src/Filament/Clusters/Property/Resources/EventResource/Pages/CreateEvent.php
@@ -5,10 +5,23 @@ namespace Bishopm\Church\Filament\Clusters\Property\Resources\EventResource\Page
 use Bishopm\Church\Filament\Clusters\Property\Resources\EventResource;
 use Bishopm\Church\Models\Diaryentry;
 use Filament\Resources\Pages\CreateRecord;
+use Spatie\GoogleCalendar\Event;
 
 class CreateEvent extends CreateRecord
 {
     protected static string $resource = EventResource::class;
+
+    protected function beforeCreate(): void
+    {
+        $day=substr($this->data['eventdate'],0,10);
+        $time=substr($this->data['eventdate'],11);
+        $event = new Event;
+        $event->name = $this->data['event'];
+        $event->description = "group_id:0";
+        $event->startDateTime = \Carbon\Carbon::createFromFormat('Y-m-d H:i',$day . " " . $time)->setTimezone('UTC');
+        $event->endDateTime = \Carbon\Carbon::createFromFormat('Y-m-d H:i',$day . " " . $this->data['endtime'])->setTimezone('UTC');
+        $event->save();
+    }
 
     protected function afterCreate(): void
     {

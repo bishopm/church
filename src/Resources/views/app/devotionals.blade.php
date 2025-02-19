84f4932c7c07e6b874799d9df9be046bbd27bacc
Michael Bishop <michael@bishop.net.za>
2025-02-19 08:38:47 +0200

    Add Bible in a Year

--- src/Resources/views/app/devotionals.blade.php
+++ src/Resources/views/app/devotionals.blade.php
@@ -6,6 +6,11 @@
             </button>
         </li>
         <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-biay-tab" data-bs-toggle="pill" data-bs-target="#pills-biay" type="button" role="tab" aria-controls="pills-biay" aria-selected="false">
+                <i class="bi bi-book"> </i>Bible in a Year
+            </button>
+        </li>
+        <li class="nav-item" role="presentation">
             <button class="nav-link" id="pills-prayers-tab" data-bs-toggle="pill" data-bs-target="#pills-prayers" type="button" role="tab" aria-controls="pills-prayers" aria-selected="false">
                 <i class="bi bi-person-arms-up"> </i>Prayers
             </button>
@@ -16,6 +21,11 @@
             <div class="card p-3">
                 <h4>{!!$ffdl_title!!}</h4>
                 {!!$ffdl!!}
+            </div>
+        </div>
+        <div class="tab-pane fade show" id="pills-biay" role="tabpanel" aria-labelledby="pills-biay-tab">
+            <div class="card p-3">
+                <iframe src="https://bible.alpha.org/en/express/{{1+date('z')}}/index.html" frameborder="0" height="700px" allowfullscreen></iframe>
             </div>
         </div>
         <div class="tab-pane fade show" id="pills-prayers" role="tabpanel" aria-labelledby="pills-prayers-tab">

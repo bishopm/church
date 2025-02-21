fd840d9cdac84783527ed62f4b46980136e96bc2
Michael Bishop <michael@bishop.net.za>
2025-02-21 13:52:45 +0200

    Giving image

--- src/Resources/views/app/page.blade.php
+++ src/Resources/views/app/page.blade.php
@@ -2,7 +2,7 @@
     <h1>{{$page->title}}</h1>
     <div style="min-height: 270px;">
         @if ($page->image)
-            <img style="float:right; padding-left:10px;" height="250px" src="{{url('/storage/' . $page->image)}}" alt="Image" class="rounded">
+            <img class="img-fluid" src="{{url('/storage/' . $page->image)}}" alt="Image" class="rounded">
         @endif
         {!!$page->content!!}<br><br>
     </div>

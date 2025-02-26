78b907f97ce7e60884c1a39a35a1139c1c4f0424
Michael Bishop <michael@bishop.net.za>
2025-02-26 14:52:45 +0200

    Tidy up member widget

--- src/Filament/Widgets/NewMembers.php
+++ src/Filament/Widgets/NewMembers.php
@@ -14,7 +14,6 @@ class NewMembers extends Widget
 
     function mount() {
         $this->memberdata['individuals']=Individual::orderBy('created_at','DESC')->take(5)->get();
-        //$this->memberdata['duplicates']=Individual::select(DB::raw("SELECT CONCAT('firstname','surname') as 'fullname', COUNT(fullname) FROM individuals GROUP BY fullname HAVING COUNT(*) > 1;"))->get();
         $this->memberdata['duplicates']=DB::table('individuals')->select('firstname','surname', DB::raw('COUNT(*) as `count`'))->groupBy('firstname', 'surname')->having('count', '>', 1)->get()->take(15);
         $this->memberdata['whatsapp']=setting('messages.welcome_whatsapp');
     }

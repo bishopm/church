e6589e8297944285f6402a0829311e518d1395ac
Michael Bishop <michael@bishop.net.za>
2025-03-26 11:47:37 +0200

    Typo in app service page

--- src/Resources/views/app/team.blade.php
+++ src/Resources/views/app/team.blade.php
@@ -1,5 +1,5 @@
 <x-church::layouts.app pageName="Service team">
     <h1>{{$team->groupname}}</h1>
     <p>{{$team->description}}</p>
-    <small><a href="{{route('app.contact')}}">Contact us</a> if you'd interested in serving in this team.</small>
+    <small><a href="{{route('app.contact')}}">Contact us</a> if you'd be interested in serving in this team.</small>
 </x-church::layout>                

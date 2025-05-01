0d62271e73474542c5ffc10d3108d0b60f731941
Michael Bishop <michael@bishop.net.za>
2025-05-01 11:33:08 +0200

    Tweak preaching widget

--- src/Resources/views/widgets/diary.blade.php
+++ src/Resources/views/widgets/diary.blade.php
@@ -24,7 +24,7 @@
                     <table class="table">
                         <tr><th>Date</th><th>Time</th><th>Preacher</th></tr>
                         @foreach ($widgetdata['plans'] as $plan)
-                            <tr><td>{{$plan->servicedate}}</td><td style="padding-left:5px; padding-right:5px;">{{$widgetdata['services'][$plan->service_id]}}</td><td><b>{{$plan->person->title ?? '' }} {{$plan->person->firstname ?? '' }} {{$plan->person->surname ?? '' }}</b></td></tr>
+                            <tr><td>{{$plan->servicedate}}</td><td style="padding-left:5px; padding-right:5px;">{{$widgetdata['services'][$plan->service_id]}}</td><td><b>{{$plan->person->firstname ?? '' }} {{$plan->person->surname ?? '' }}</b></td></tr>
                         @endforeach
                     </table>
                 @else

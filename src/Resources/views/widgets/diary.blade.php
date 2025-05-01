9596c58dfe67f826b637bee32e767cbc8bc84263
Michael Bishop <michael@bishop.net.za>
2025-05-01 11:42:02 +0200

    Widget design

--- src/Resources/views/widgets/diary.blade.php
+++ src/Resources/views/widgets/diary.blade.php
@@ -22,7 +22,7 @@
             <x-filament::section>
                 @if (count($widgetdata['plans']))
                     <table class="table">
-                        <tr><th>Date</th><th>Time</th><th>Preacher</th></tr>
+                        <tr><td><b>Date</b></td><td><b>Time</b></td><td><b>Preacher</b></td></tr>
                         @foreach ($widgetdata['plans'] as $plan)
                             <tr><td>{{$plan->servicedate}}</td><td style="padding-left:5px; padding-right:5px;">{{$widgetdata['services'][$plan->service_id]}}</td><td><b>{{$plan->person->firstname ?? '' }} {{$plan->person->surname ?? '' }}</b></td></tr>
                         @endforeach

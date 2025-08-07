844739863d60f304f8d12752f9b913e31369d185
Michael Bishop <michael@bishop.net.za>
2025-08-07 12:48:31 +0200

    Conditional image

--- src/Resources/views/web/project.blade.php
+++ src/Resources/views/web/project.blade.php
@@ -1,9 +1,11 @@
 <x-church::layouts.web pageName="Blog post">
     <h3 class="text-uppercase">{{$project->title}}</h3>
     <div class="pb-3">
-        <div>
-            <img style="float:left; height:200px; padding-right:15px;" src="{{url('/storage/' . $project->image)}}">
-        </div>
+        @if ($project->image)
+            <div>
+                <img style="float:left; height:200px; padding-right:15px;" src="{{url('/storage/' . $project->image)}}">
+            </div>
+        @endif
         {!!$project->description!!}<br>
     </div>
 </x-church::layout>                

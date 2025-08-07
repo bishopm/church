8cbe188ccb1b8024c69e0987f2c2a3a5c8c0bc61
Michael Bishop <michael@bishop.net.za>
2025-08-07 13:55:52 +0200

    Allow add user password

--- src/Filament/Clusters/Settings/Resources/UserResource.php
+++ src/Filament/Clusters/Settings/Resources/UserResource.php
@@ -35,7 +35,10 @@ class UserResource extends Resource
                     ->required()
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\Select::make('roles')->multiple()->relationship('roles', 'name')
+                Forms\Components\Select::make('roles')->multiple()->relationship('roles', 'name'),
+                Forms\Components\TextInput::make('password')
+                    ->hiddenOn('edit')
+                    ->password()
             ]);
     }
 

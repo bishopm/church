6029c2c2f563175e0d6329b5f04c317b0f8d54a6
Michael Bishop <michael@bishop.net.za>
2025-08-12 12:51:39 +0200

    Update venue migration

--- src/Database/migrations/create_venues_table.php
+++ src/Database/migrations/create_venues_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
             $table->increments('id')->unsigned();
             $table->string('venue', 255);
             $table->string('slug', 199);
+            $table->text('description');
+            $table->decimal('width',4,2);
+            $table->decimal('length',4,2);
+            $table->string('image', 199)->nullable();
             $table->tinyinteger('publish');
             $table->integer('resource');
+            $table->string('colour', 10)->nullable();
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
         });

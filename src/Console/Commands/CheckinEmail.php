5784efa39ae95de5cc414a52835e7ce7a064399f
Michael Bishop <michael@bishop.net.za>
2025-09-03 15:26:59 +0200

    Fix checkin mail

--- src/Console/Commands/CheckinEmail.php
+++ src/Console/Commands/CheckinEmail.php
@@ -3,6 +3,7 @@
 namespace Bishopm\Church\Console\Commands;
 
 use Bishopm\Church\Mail\ChurchMail;
+use Bishopm\Church\Models\Attendance;
 use Bishopm\Church\Models\Group;
 use Bishopm\Church\Models\Individual;
 use Illuminate\Console\Command;
@@ -31,24 +32,49 @@ class CheckinEmail extends Command
     public function handle()
     {
         Log::info('Preparing Check in email on ' . date('Y-m-d H:i'));
-        $sixweeks=date('Y-m-d',strtotime('-6 weeks'));
         $data=array();
-        $data['never']=array();
-        $missings=Individual::with(['attendances'=> function($q) use ($sixweeks) { $q->where('attendancedate','<',$sixweeks)->orderBy('attendancedate');}])->orderBy('surname','ASC')->get();
-        foreach ($missings as $missing){
-            if (count($missing->attendances)>0){
-                $data['attended'][]=$missing;
-            } else {
-                $data['never'][]=$missing;
+        $data['never']=array();      
+        $sixWeeks  = now()->subWeeks(6);
+        $sixMonths = now()->subMonths(6);
+        $individuals = Individual::select('individuals.*')
+            ->addSelect([
+                'last_attended' => Attendance::selectRaw('MAX(attendancedate)')
+                    ->whereColumn('individual_id', 'individuals.id')
+            ])
+            ->orderBy('surname', 'ASC')
+            ->get();
+
+        $data = [
+            'never'  => [],
+            'over_six_months' => [],
+            'over_six_weeks'  => [],
+        ];
+
+        foreach ($individuals as $individual) {
+            $last = $individual->last_attended;
+
+            if (is_null($last)) {
+                // 1. Never attended
+                $data['never'][] = $individual;
+            } elseif ($last < $sixMonths) {
+                // 2. Attended, but not for over 6 months
+                $data['over_six_months'][] = $individual;
+            } elseif ($last < $sixWeeks) {
+                // 3. Attended, but not in the last 6 weeks (but within 6 months)
+                $data['over_six_weeks'][] = $individual;
             }
         }
         $message="The following people have never used a nametag at a service:\n\n";
         foreach ($data['never'] as $never){
             $message.=$never->firstname . " " . $never->surname . ". Cellphone: " . $never->cellphone . "<br>";
         }
-        $message.="\n\n" . "The following people have not used a nametag at a service since " . $sixweeks . ":\n\n";
-        foreach ($data['attended'] as $attended){
+        $message.="\n\n" . "The following people have not used a nametag at a service for the last six months:\n\n";
+        foreach ($data['over_six_months'] as $attended){
             $message.=$attended->firstname . " " . $attended->surname . ". Cellphone: " . $attended->cellphone . " (Last seen: " . $attended->lastseen . ")<br>";
+        }
+        $message.="\n\n" . "The following people have used a nametag in the last six months, but not in the last six weeks :\n\n";
+        foreach ($data['over_six_weeks'] as $away){
+            $message.=$away->firstname . " " . $away->surname . ". Cellphone: " . $away->cellphone . " (Last seen: " . $away->lastseen . ")<br>";
         }
         // Send to followup group
         $setting=intval(setting('automation.followup_group'));

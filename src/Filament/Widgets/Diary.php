aea846f2b9c0daf4e420dbc342022bf88d0f1f51
Michael Bishop <michael@bishop.net.za>
2025-09-18 11:32:56 +0200

    Fix Diary widget

--- src/Filament/Widgets/Diary.php
+++ src/Filament/Widgets/Diary.php
@@ -2,11 +2,12 @@
 
 namespace Bishopm\Church\Filament\Widgets;
 
+use Bishopm\Church\Models\Event;
 use Bishopm\Church\Models\Plan;
 use Carbon\Carbon;
 use Filament\Widgets\Widget;
 use Illuminate\Support\Facades\DB;
-use Spatie\GoogleCalendar\Event;
+
 
 class Diary extends Widget
 {
@@ -23,21 +24,12 @@ class Diary extends Widget
         $today = date('Y-m-d');
         $nextweek = date('Y-m-d',strtotime('+10 days'));
         $this->widgetdata['plans']=Plan::with('person')->whereIn('service_id',array_keys($this->widgetdata['services']))->where('servicedate','>=',$today)->where('servicedate','<=',$nextweek)->orderBy('servicedate','ASC')->get();
-        $events=Event::get(new Carbon($today),new Carbon(date('Y-m-d',strtotime('+1 month'))));
+        $this->widgetdata['events']=[];
+        $events=Event::where('eventdate','>=',$today)->where('eventdate','<=',$nextweek)->orderBy('eventdate','ASC')->get();
         foreach ($events as $event){
-            if ($event->startDateTime){
-                $this->widgetdata['events'][]=[
-                    'name' => $event->name,
-                    'date' => date('D j M',strtotime($event->startDateTime)),
-                    'time' => date('H:i',strtotime($event->startDateTime))
-                ];
-            } else {
-                $this->widgetdata['events'][]=[
-                    'name' => $event->name,
-                    'date' => date('D j M',strtotime($event->startDate)),
-                    'time' => ''
-                ];
-            }
+            $event->dayofweek=Carbon::parse($event->startdate)->format('D');
+            $event->starttime=Carbon::parse($event->starttime)->format('H:i');
+            $this->widgetdata['events'][]=$event;
         }
     }
 
